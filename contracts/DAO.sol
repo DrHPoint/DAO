6fef59b17a61e70aa00a750230dc5c61e28036d5
Павел Хрущёв <charvelcjc1113712@outlook.com>
2021-12-14 20:44:40 +0800

    All functions. Expected problem with final function

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -1,83 +1,142 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DAO {
 
-    struct Proposal { 
-        bytes byteCode;
-        string description;
-        address recipient;
-        uint256 minimumQuorum;
-        uint256 minimumAccept;
-        mapping (address => Account) accounts;
-        bool status;
-        uint256 voteAccept;
-        uint256 voteAgainst;
-        string dateBeggining;
-        string dateEnding;
-    }
-
-    struct Account {
-        uint256 balance;
-        uint256 weight;
-        bool vote;
-    }
-
     mapping (address => uint256) private tokens;
     mapping (address => bool) private locks;
     mapping (uint256 => Proposal) public proposals;
     address public tokenAddress;
     address public owner;
-    uint256 public currentProposal;
+    uint256 public currentProposal = 0;
+    uint256 public minimumAccept;
+    uint256 private i;
+    uint256 private duration;
 
 
-    constructor(address _tokenAddress) {
+    constructor(address _tokenAddress, uint256 _minimumAccept, uint256 _duration) {
         owner = msg.sender;
         tokenAddress = _tokenAddress;
+        minimumAccept = _minimumAccept;
+        duration = _duration * 1 days;
     }
 
-    function newProposal(bytes memory _byteCode, string memory _description, address _recipient, uint256 procentQuorum, uint256 _minimumAccept) public {
+    modifier forCurrentProposal (uint256 _idProposal) {
+        require(_idProposal <= currentProposal, "Proposal doesn't exist");
+        require(proposals[_idProposal].status, "Proposal is over");
+        _;
+   }    
+
+    event NewProposal(uint256 _indexProposal, bytes _byteCode, string _description, address indexed _recipient);
+    event CloseProposal(uint256 _indexProposal, uint256 _voteAccept, uint256 _voteAgainst);
+    event Vote(uint256 _indexProposal, address _voting, uint256 _weight, bool _accept);
+
+    
+    struct Proposal { 
+        bytes byteCode;
+        string description;
+        address recipient;
+        uint256 minimumQuorum;
+        uint256 controlPackage;
+        mapping (address => Account) accounts;
+        bool status;
+        uint256 voteAccept;
+        uint256 voteAgainst;
+        uint256 dateBeggining;
+        uint256 dateEnding;
+    }
+
+    struct Account {
+        uint256 votesIn;
+        uint256 weight;
+        bool vote;
+    }
+
+    function createProposal(bytes memory _byteCode, string memory _description, address _recipient) public {
+        Proposal storage proposal = proposals[currentProposal];
+        proposal.byteCode = _byteCode;
+        proposal.description = _description;
+        proposal.recipient = _recipient;
+        proposal.minimumQuorum = IERC20(tokenAddress).totalSupply() * 3 / 10;
+        proposal.controlPackage = IERC20(tokenAddress).totalSupply() / 2 + 1;
+        proposal.status = true;
+        proposal.dateBeggining = block.timestamp;
+        proposal.dateEnding = proposal.dateBeggining + duration;
+        emit NewProposal(currentProposal, _byteCode, _description, _recipient);
         currentProposal++;
-        proposals[currentProposal].byteCode = _byteCode;
-        proposals[currentProposal].description = _description;
-        proposals[currentProposal].recipient = _recipient;
-        proposals[currentProposal].minimumQuorum = IERC20(tokenAddress).totalSupply() * procentQuorum;
-        proposals[currentProposal].minimumAccept = _minimumAccept;
-        proposals[currentProposal].voteAccept = 0;
-        proposals[currentProposal].voteAgainst = 0;
-        proposals[currentProposal].dateBeggining = "";
-        proposals[currentProposal].dateEnding = "";
     }
     
-    function deposite(uint256 amount) public {
-        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount), "Deposite failed");
-        tokens[msg.sender] = amount;
+    function deposite(uint256 _amount) public {
+        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), _amount), "Deposite failed");
+        tokens[msg.sender] = _amount;
         locks[msg.sender] = true;
     }
     
-    function depositeBack(uint256 amount) public {
-        require(tokens[msg.sender] >= amount, "Not enough tokens");
-        require(locks[msg.sender] = true, "Not all voting completed");
-        require(IERC20(tokenAddress).transfer(address(this), amount), "Deposite failed");
-        tokens[msg.sender] -= amount;
+    function withdraw(uint256 _amount) public {
+        require(tokens[msg.sender] >= _amount, "Not enough tokens");
+        checkAddress(msg.sender);
+        require(IERC20(tokenAddress).transfer(msg.sender, _amount), "Withdraw failed");
+        tokens[msg.sender] -= _amount;
     }
 
-    function delegate(uint256 idProposal, address voting, uint256 amount) public{
-        require(proposals[idProposal].accounts[msg.sender].vote = false, "Voting has already taken place");
-        require(proposals[idProposal].accounts[voting].vote = false, "The trustee has already voted");
-        require(tokens[msg.sender] - proposals[idProposal].accounts[msg.sender].balance >= amount, "Not enough tokens");
-        proposals[idProposal].accounts[voting].weight += amount;
-        proposals[idProposal].accounts[msg.sender].balance += amount;
+    function delegate(uint256 _idProposal, address _voting) public forCurrentProposal(_idProposal){
+        require(proposals[_idProposal].dateEnding > block.timestamp, "Proposal is over");
+        Account storage account = proposals[_idProposal].accounts[msg.sender];
+        Account storage dAccount = proposals[_idProposal].accounts[_voting];
+        require(dAccount.vote == false, "The delegee has already voted");
+        require(tokens[msg.sender] - account.votesIn > 0, "Not enough tokens");
+        dAccount.weight += tokens[msg.sender] - account.votesIn;
+        account.votesIn = tokens[msg.sender];
+        locks[msg.sender] = false;
     }
     
-    function vote(uint256 idProposal) public {
-        require(proposals[idProposal].accounts[msg.sender].vote = false, "Voting has already taken place");
-        
+    function vote(uint256 _idProposal, bool _accept) public {
+        delegate(_idProposal, msg.sender);
+        Account storage account = proposals[_idProposal].accounts[msg.sender];
+        if (_accept) 
+            proposals[_idProposal].voteAccept += account.weight;
+        else
+            proposals[_idProposal].voteAgainst += account.weight;
+        account.vote = true;
+        emit Vote(_idProposal, msg.sender, account.weight, _accept);
+        checkVotes(_idProposal);
     }
-    
-    
-    
+
+    function checkVotes(uint256 _idProposal) private {
+        Proposal storage proposal = proposals[_idProposal];
+        if (proposal.voteAccept + proposal.voteAgainst >= proposal.minimumQuorum) 
+        {
+            if (((proposal.voteAccept + proposal.voteAgainst) * minimumAccept / 100 <= proposal.voteAccept) 
+                    || (proposal.voteAccept >= proposal.controlPackage))
+            {
+                proposal.recipient.call(proposal.byteCode);
+                endProposal(_idProposal);
+            } 
+            else if (proposal.voteAgainst >= proposal.controlPackage) 
+                endProposal(_idProposal);
+        } 
+        else if (proposals[_idProposal].dateEnding < block.timestamp) 
+            endProposal(_idProposal);
+    }
+
+    function endProposal(uint256 _idProposal) private {
+        Proposal storage proposal = proposals[_idProposal];
+        proposal.status = false;
+                emit CloseProposal(_idProposal, proposal.voteAccept, proposal.voteAgainst);
+    }
+
+    function checkEnding(uint256 _idProposal) public forCurrentProposal(_idProposal){
+        require(proposals[_idProposal].dateEnding < block.timestamp, "Proposal isnt over");
+        checkVotes(_idProposal);
+    }
+
+    function checkAddress(address _asking) private {
+        for (i = 1; i < currentProposal; i++) 
+            if (proposals[i].status) 
+                require(proposals[i].accounts[_asking].votesIn > 0, "Not all voting completed");
+        locks[_asking] = true;
+    }
 }
