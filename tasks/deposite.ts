34c40a34c0f2531fb0a1f18ca723a969e9ea508d
Павел Хрущёв <charvelcjc1113712@outlook.com>
2021-12-20 04:10:36 +0800

    some fix

--- tasks/deposite.ts
+++ tasks/deposite.ts
@@ -10,13 +10,12 @@ import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-web3";
 
 task("deposite", "deposite tokens to DAO")
-.addParam("addr", "user address")
 .addParam("amount", "amount token to deposite")
 .setAction(async (taskArgs, hre) =>{
   const [addr1, addr2, ...addrs] = await hre.ethers.getSigners();
 
   const dao = await hre.ethers.getContractAt("DAO", DAO_ADDR as string);
-  await dao.connect(taskArgs.addr).deposite(parseUnits(taskArgs.amount, 18));
+  await dao.connect(addr1).deposite(parseUnits(taskArgs.amount, 18));
 
   console.log('deposite task Done!'); 
 });
